d6522b5fd3349b434a3428fa6bbb6cbe426b37d1
Ching-Yen Pai <ricky@aniarc.com>
2013-03-10 18:23:31 -0700

    moved images, organized cards

--- app/helpers/main_helper.rb
+++ app/helpers/main_helper.rb
@@ -1,2 +1,5 @@
 module MainHelper
+  def render_card(front, back, counter)
+    render partial: 'card', locals: {front_url: "cards/front/#{front}.jpg", back_url: "cards/back/#{back}.jpg", counter: counter}
+  end
 end
